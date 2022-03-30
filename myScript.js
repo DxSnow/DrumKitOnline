fe494bf4de557490aa7732c037330ae75b7d10d0
DxSnow <62160409+DxSnow@users.noreply.github.com>
2022-03-29 18:46:32 -0700

    initial commit

---
+++ myScript.js
@@ -0,0 +1,20 @@
+// listen to the whole page, if a key is pressed, pass that event to a function that renders predefined effects and sound
+document.addEventListener("keydown",playAudio)
+// the whole html file is the "document"
+
+document.querySelectorAll('.key').forEach(key => key.addEventListener('transitionend', backToNormal))
+
+function playAudio(e){
+  const audio = document.querySelector(`audio[data-key = ${e.key}]`)
+  // the selected html part is an "element"
+  // keyEvent.key returns the key's value. e.keycode is deprecated, so use e.key instead
+  const key = document.querySelector(`div[data-key = ${e.key}]`)
+  if (!audio || !key) return;
+  key.classList.add('playing')
+  audio.currentTime = 0
+  audio.play()
+}
+function backToNormal(e){
+  // if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing')
+}
